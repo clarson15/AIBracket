d797b7ac9f030618889627a6f10c8ec50ba4562c
gendelmansa <gendelmansa@gmail.com>
2019-02-10 20:34:06 -0500

    added login page design with forms set up (no front end api calls yet)

--- AIBracket.Web/ClientApp/src/app/app.module.ts
+++ AIBracket.Web/ClientApp/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router'
+import { MatCardModule, MatExpansionModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatFormField} from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -22,6 +25,16 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    //Forms
+    ReactiveFormsModule,
+    //Material
+    MatExpansionModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    //Routes
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
