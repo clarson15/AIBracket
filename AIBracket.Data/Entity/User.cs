44d1102913c93176703ee787095400e0c63fac66
Charles Larson <charles.w.larson@wmich.edu>
2019-02-11 23:41:57 -0500

    add basic authenitcation in login controller

---
+++ AIBracket.Data/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIBracket.Data.Entity
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string salt { get; set; }
+        public int uid { get; set; }
+        public string create_date { get; set; }
+        public string token { get; set; }
+    }
+}
