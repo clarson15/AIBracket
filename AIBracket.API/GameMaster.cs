104afcfcd338f1a296d4acf7a78c90bce2c61f1d
gendelmansa <gendelmansa@gmail.com>
2019-02-26 21:28:20 -0500

    api branch started

---
+++ AIBracket.API/GameMaster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIBracket.API
+{
+    public static class GameMaster
+    {
+        private static List<Object> games; // current games running (playing atm)
+        private static List<Object> players; // current connected players
+        private static Boolean isRunning; 
+
+        public static void Initialize() { // constructor for our games list and players
+            games = new List<Object>(); // <- create functions to add players and games
+            players = new List<Object>();
+            isRunning = false;
+        }
+
+        public static void AddGame(Object game) { // adds a new thread with the game object into our thread list
+            games.Add(game);
+        }
+
+        public static void AddPlayer(Object player) {
+            players.Add(player);
+        }
+        public static int GetPlayerCount() {
+            return players.Count; // for creating a new game
+        }
+        public static void InputCommand(string command)
+        {
+             // this will parse commands
+        }
+        public static async void Run() {
+            isRunning = true;
+            while (isRunning) { 
+
+                /*
+                1. Gets player input, if any
+                2. Update games with input from players
+                3. Update the players with current game state, loop back to 1
+                (^^^this will happen ever second -> thread.sleep() to acheive this)
+                4. adds a new game for new players if needed
+                */
+            }
+            // restart? print out debug stuff?
+        }
+
+        public static 
+    }
+}
--- AIBracket.API/Program.cs
+++ AIBracket.API/Program.cs
@@ -1,24 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using AIBracket.GameLogic;
 namespace AIBracket.API
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+    class TcpHelper {
+        private static TcpListener listener { get; set; }
+        private static bool accept { get; set; } = false;
+
+        public static void StartServer(int port) {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            GameMaster.Initialize(); // create game master
+            GameMaster.Run();
+            /* 
+            initialize game master: <- this will handle all of our games and solo queue
+                1.setup up the game types
+                2.add connected player to match queue
+                3.when 2 players or more connected start a thread with a game in it between the 2 players
+                5.wait until the game finish and get the final results. 
+                6.put players back into match queue and start back at step 3
+                7.take match data and replay data and push it to the database
+                8.END THREAD
+             */
+            listener = new TcpListener(address, port);
+            listener.Start();
+            accept = true;
+
+            Console.WriteLine($"Server started. Listening to TCP clients at 127.0.0.1:{port}");
+        }
+        public static void Listen()
+        {
+            if (listener != null && accept)
+            {
+
+                // Continue listening.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client...");
+                    var clientTask = listener.AcceptTcpClientAsync(); // Get the client  
+
+                    if (clientTask.Result != null)
+                    {
+                        // create thread here
+                        Console.WriteLine("Client connected. Waiting for data.");
+                        var client = clientTask.Result;
+                        string message = "";
+
+                        while (message != null && !message.StartsWith("quit"))
+                        {
+                            byte[] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
+                            client.GetStream().Write(data, 0, data.Length);
+
+                            byte[] buffer = new byte[1024];
+                            client.GetStream().Read(buffer, 0, buffer.Length);
+
+                            message = Encoding.ASCII.GetString(buffer);
+                            Console.WriteLine(message); 
+                        }
+                        Console.WriteLine("Closing connection.");
+                        client.GetStream().Dispose();
+                    }
+                }
+            }
+        }
+        public static void Main(string[] args) {
+            StartServer(8000);
+
+
+        }
     }
-}
+}  
