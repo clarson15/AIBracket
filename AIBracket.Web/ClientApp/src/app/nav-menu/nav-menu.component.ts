17ade8ec09a5182f2b4849ecdd298192e6f8c4c2
Charles Larson <charles.w.larson@wmich.edu>
2019-04-16 01:48:38 -0400

    forgot to update profile button

--- AIBracket.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ AIBracket.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -24,7 +24,7 @@ export class NavMenuComponent implements OnInit {
   }
 
   viewProfile() {
-    this.router.navigate(['/profile']);
+    this.router.navigate(['/profile', this.account.id]);
   }
 
   getDisplayName() {
